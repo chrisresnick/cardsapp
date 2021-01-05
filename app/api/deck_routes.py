ff56ba80ceef45703cdc3de36db2e7cea71fd264
Chris Resnick <chris.resnick@gmail.com>
2021-01-05 13:37:46 -0500

    add card

--- app/api/deck_routes.py
+++ app/api/deck_routes.py
@@ -5,8 +5,8 @@ from app.models import db, Deck, Card
 deck_routes = Blueprint('deck', __name__)
 
 
-@login_required
 @deck_routes.route("/", methods=["POST"])
+@login_required
 def createDeck():
     name = request.json["name"]
     owner = current_user.id
@@ -15,6 +15,16 @@ def createDeck():
     db.session.commit()
     return jsonify(newDeck.to_dict())
 
+
 @deck_routes.route("/<int:id>/cards", methods=["POST"])
+@login_required
 def addCard(id):
-    pass
+    deck = Deck.query.get(id)
+    question = request.json["question"]
+    answer = request.json["answer"]
+    if deck.ownerId != current_user.id:
+        return {"errors": ["You cannot add to a deck that you don't own"]}, 401
+    newCard = Card(deckId=deck.id, question=question, answer=answer)
+    deck.cards.append(newCard)
+    db.session.commit()
+    return jsonify(newCard.to_dict())
--- app/models/card.py
+++ app/models/card.py
@@ -11,3 +11,12 @@ class Card(db.Model):
     nextShow = db.Column(db.DateTime, default=datetime.now())
 
     deck = db.relationship("Deck", back_populates="cards")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "deckId": self.deckId,
+            "question": self.question,
+            "answer": self.answer,
+            "nextShow": self.nextShow
+        }
