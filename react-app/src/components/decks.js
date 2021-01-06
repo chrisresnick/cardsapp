a17f1532720dc4cc817379e0b8d41ee996971f8d
Chris Resnick <chris.resnick@gmail.com>
2021-01-06 16:25:59 -0500

    add scrollbar

--- react-app/src/components/decks.js
+++ react-app/src/components/decks.js
@@ -1,4 +1,5 @@
-import { Box, Button, SimpleGrid, Text, Heading, Flex, useDisclosure, Modal, ModalContent, ModalOverlay, Input} from "@chakra-ui/react";
+import { Box, IconButton, Button, SimpleGrid, Text, Heading, Flex, useDisclosure, Modal, ModalContent, ModalOverlay, Input} from "@chakra-ui/react";
+import {ArrowUpIcon, ArrowDownIcon} from '@chakra-ui/icons';
 import React, {useState, useContext, useEffect} from "react";
 import {useHistory} from 'react-router-dom';
 import {UserContext} from "./context";
@@ -31,13 +32,15 @@ const PlaceHolder = ({decks, setDecks}) => {
                 <ModalContent mx="auto" my="auto">
                     <Box textAlign="center" p="2">
                         <Heading>Create New Deck</Heading>
-                        <Input
-                            placeholder="Deck Name"
-                            onChange={e => setName(e.target.value)}
-                            value={name}
-                            m="2"
-                        />
-                        <Button w="100%" onClick={submitDeck}>Create</Button>
+                        <form onSubmit={submitDeck}>
+                            <Input
+                                placeholder="Deck Name"
+                                onChange={e => setName(e.target.value)}
+                                value={name}
+                                m="2"
+                            />
+                            <Button w="100%" onClick={submitDeck}>Create</Button>
+                        </form>
                     </Box>
                 </ModalContent>
             </Modal>
@@ -60,11 +63,37 @@ const Deck = ({deck}) => {
     )
 }
 
+export const Scrollbar = ({startIdx, setStartIdx, lim}) => {
+    return (
+        <Flex
+            direction="column"
+            borderTop="1px" borderRight="1px" borderBottom="1px"
+            borderColor="gray.300"
+            h="80vh"
+            justify="space-between"
+        >
+            <IconButton
+                onClick={e=>setStartIdx(startIdx-3)}
+                aria-label="scrool up"
+                icon={<ArrowUpIcon/>}
+                disabled={startIdx < 3}
+            />
+            <IconButton
+                onClick={e=>setStartIdx(startIdx+3)}
+                aria-label="scrool down"
+                icon={<ArrowDownIcon/>}
+                disabled={startIdx > lim}
+            />
+        </Flex>
+    );
+
+};
+
 const Decks = () => {
     const {user, setUser} = useContext(UserContext);
     const [decks, setDecks] = useState([]);
     const [startIdx, setStartIdx] = useState(0);
-    const extras = [0,1,2,3,4,5,6,7,8,9,10]
+    const extras = [0,1,2,3,4,5,6,7,8]
     useEffect(() => {
         (async ()=>{
             let res = await fetch(`/api/users/${user.id}/decks`);
@@ -73,12 +102,15 @@ const Decks = () => {
         })();
     }, [user.id])
     return (
-        <Box width="100%" pt={10}>
-            <SimpleGrid border="1px" borderColor="gray.300" p={10} columns={3} spacing={10} w="80%" h="80vh" mx="auto">
-                {decks.slice(startIdx, startIdx+9).map(deck => <Deck key={`deck#${deck.id}`} deck={deck}/>)}
-                {extras.slice(0, (9-(decks.length-startIdx))).map((_, idx) => <PlaceHolder key={`ph${idx}`} decks={decks} setDecks={setDecks}/>)}
-            </SimpleGrid>
-        </Box>
+        <Flex width="100%" pt={10} justify="center">
+            <Flex width="80%">
+                <SimpleGrid borderLeft="1px" borderTop="1px" borderBottom="1px" borderColor="gray.300" p={10} columns={3} spacing={10} w="90%" h="80vh">
+                    {decks.slice(startIdx, startIdx+9).map(deck => <Deck key={`deck#${deck.id}`} deck={deck}/>)}
+                    {extras.slice(0, (9-(decks.length-startIdx))).map((_, idx) => <PlaceHolder key={`ph${idx}`} decks={decks} setDecks={setDecks}/>)}
+                </SimpleGrid>
+                <Scrollbar startIdx={startIdx} setStartIdx={setStartIdx} lim={decks.length}/>
+            </Flex>
+        </Flex>
     );
 
 }
