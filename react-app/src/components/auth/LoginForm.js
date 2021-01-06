03df5369e3142902490c04f17d96c1fb56c54ff3
Chris Resnick <chris.resnick@gmail.com>
2021-01-06 11:48:01 -0500

    start decks fe

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { login } from "../../services/auth";
-import UserContext from "../context";
+import {UserContext} from "../context";
 
 
 const LoginForm = ({ authenticated, setAuthenticated}) => {
--- react-app/src/components/decks.js
+++ react-app/src/components/decks.js
@@ -1,7 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useContext, useEffect} from "react";
+import {UserContext} from "./context";
 
 const Deck = () => {
+    const {user, setUser} = useContext(UserContext);
     const [decks, setDecks] = useState([])
+
+
 }
 
 export default Deck;
