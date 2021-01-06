8e931158d164e686a686a747c510e950b26a3f2f
Chris Resnick <chris.resnick@gmail.com>
2021-01-06 14:03:29 -0500

    create deck from modal

--- react-app/src/components/decks.js
+++ react-app/src/components/decks.js
@@ -1,10 +1,26 @@
-import { Box, Button, SimpleGrid, Text, Heading, Flex, useDisclosure, Modal, ModalContent, ModalOverlay} from "@chakra-ui/react";
+import { Box, Button, SimpleGrid, Text, Heading, Flex, useDisclosure, Modal, ModalContent, ModalOverlay, Input} from "@chakra-ui/react";
 import React, {useState, useContext, useEffect} from "react";
 import {useHistory} from 'react-router-dom';
 import {UserContext} from "./context";
 
-const PlaceHolder = () => {
+const PlaceHolder = ({decks, setDecks}) => {
+    const [name, setName] = useState("");
     const {isOpen, onOpen, onClose} = useDisclosure();
+
+    const submitDeck = async e => {
+        e.preventDefault();
+        let res = await fetch("/api/decks/", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+              },
+            body: JSON.stringify({name})
+        })
+        res = await res.json();
+        setDecks([...decks, res]);
+        setName("");
+        onClose();
+    }
     return (
         <>
             <Flex h="20vh" border="1px" justify="center" align="center">
@@ -13,7 +29,15 @@ const PlaceHolder = () => {
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay/>
                 <ModalContent mx="auto" my="auto">
-                    Test
+                    <Box>
+                        <Heading>Create New Deck</Heading>
+                        <Input
+                            placeholder="Deck Name"
+                            onChange={e => setName(e.target.value)}
+                            value={name}
+                        />
+                        <Button onClick={submitDeck}>Create</Button>
+                    </Box>
                 </ModalContent>
             </Modal>
         </>
@@ -51,7 +75,7 @@ const Decks = () => {
         <Box width="100%" pt={10}>
             <SimpleGrid border="1px" borderColor="gray.300" p={10} columns={3} spacing={10} w="80%" h="80vh" mx="auto">
                 {decks.slice(startIdx, startIdx+9).map(deck => <Deck key={`deck#${deck.id}`} deck={deck}/>)}
-                {extras.slice(0, (decks.length-startIdx+7)).map(e => <PlaceHolder/>)}
+                {extras.slice(0, (9-(decks.length-startIdx))).map((_, idx) => <PlaceHolder key={`ph${idx}`} decks={decks} setDecks={setDecks}/>)}
             </SimpleGrid>
         </Box>
     );
